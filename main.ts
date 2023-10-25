7ddf7508e4e5946e59da130c4bb833f002648a05
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-10-24 17:41:25 -0700

    Follow Fireship's instructions
    Follow: https://fireship.io/lessons/typescript-nodejs-setup/

--- main.ts
+++ main.ts
@@ -1,14 +1,11 @@
-import { delay, type Page, puppeteer, SECOND } from "auto-book/deps.ts";
-import { type Env, mustEnv } from "auto-book/lib/env/mod.ts";
-import { $, URLs } from "auto-book/lib/booking/mod.ts";
+import { chromium } from "playwright";
+import { $, URLs } from "auto-book/lib/bookings";
+import "dotenv/config";
 
-if (import.meta.main) {
-  await main();
-}
+await main();
 
 async function main() {
-  const env = await mustEnv();
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await chromium.launch({ headless: false });
   const page = await browser.newPage();
   await login(page, env).catch(console.error);
   // TODO: Check if max bookings reached.
@@ -17,24 +14,24 @@ async function main() {
   // await browser.close();
 }
 
-async function login(page: Page, env: Env) {
-  await page.goto(URLs.login);
-  await page.waitForSelector($.login.username);
-  await page.type($.login.username, env.CSUF_USERNAME);
-  await page.type($.login.password, env.CSUF_PASSWORD);
-  await page.click($.login.submit);
-  await page.waitForNavigation({ waitUntil: "load" });
-  // await delay(3 * SECOND);
-}
+// async function login(page: Page, env: Env) {
+//   await page.goto(URLs.login);
+//   await page.waitForSelector($.login.username);
+//   await page.type($.login.username, env.CSUF_USERNAME);
+//   await page.type($.login.password, env.CSUF_PASSWORD);
+//   await page.click($.login.submit);
+//   await page.waitForNavigation({ waitUntil: "load" });
+//   // await delay(3 * SECOND);
+// }
 
-async function book(page: Page, env: Env) {
-  // await page.waitForSelector($.booking.numberOfStudents);
-  await page.type($.booking.numberOfStudents, env.NUMBER_OF_STUDENTS).catch(
-    console.error,
-  );
-  await page.type($.booking.additionalValidCWID, env.CWID).catch(console.error);
-  // TODO: Complete booking.
-  // TODO: Determine date and time for booking.
-  // await page.type($.book.date, );
-  // await page.select($.book.time, );
-}
+// async function book(page: Page, env: Env) {
+//   // await page.waitForSelector($.booking.numberOfStudents);
+//   await page
+//     .type($.booking.numberOfStudents, env.NUMBER_OF_STUDENTS)
+//     .catch(console.error);
+//   await page.type($.booking.additionalValidCWID, env.CWID).catch(console.error);
+//   // TODO: Complete booking.
+//   // TODO: Determine date and time for booking.
+//   // await page.type($.book.date, );
+//   // await page.select($.book.time, );
+// }
